13ee5b6272979e667209bd52c5f2465be6282c97
jskow <joshskow@gatech.edu>
2013-07-15 23:36:01 +0000

    made changes

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = (fs.readFileSync('/bitstarter/index.html'));
+  var buf = (fs.readFileSync('/bitstarter/index.html'));
   buf.toString(buf);
 });
 
