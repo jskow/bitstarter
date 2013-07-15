f24483d7cec44989556e316ce0c2e04a3c9620d7
jskow <joshskow@gatech.edu>
2013-07-15 23:16:29 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = new Buffer(fs.readFileSync(index.html));
+  var buf = new Buffer
+  buf = (fs.readFileSync(index.html));
   buf.toString(buf);
 });
 
