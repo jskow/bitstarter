e74363248fc5ea91b481370218e564fda3187047
jskow <joshskow@gatech.edu>
2013-07-15 23:14:52 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = new Buffer(fs.readFileSync(index.html));
+  buf.toString(buf);
 });
 
 var port = process.env.PORT || 5000;
