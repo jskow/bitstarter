6ab9697fe79fbe520ac976cfc1345bcf78b4c53a
jskow <joshskow@gatech.edu>
2013-07-16 00:06:13 +0000

    made changes

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buf = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync('index.html'));
-  buf.toString("utf-8");
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
